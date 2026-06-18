2861a32180a0028b888e3249e8d43a811ed3a06d
Shubham Patel <shubhampatel1573@gmail.com>
2026-06-18 19:30:23 +0530

    fix vercel deployment cache

--- .vercel/output/functions/__server.func/_ssr/hero-BW4HxANi.mjs
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/hero-BW4HxANi.js
-var hero_default = "/assets/hero-sDt-PBye.jpg";
-//#endregion
-export { hero_default as t };
--- dist/server/assets/hero-BW4HxANi.js
+++
@@ -1,4 +0,0 @@
-//#region src/assets/hero.jpg
-var hero_default = "/assets/hero-sDt-PBye.jpg";
-//#endregion
-export { hero_default as t };
